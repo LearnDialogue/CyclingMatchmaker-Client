eea59a05119d3c2464bc1a47839780a88ab74fe0
j-jurado <73544267+j-jurado@users.noreply.github.com>
2024-03-20 22:56:14 -0400

    Update RidesFeed.tsx

--- src/routes/app/RidesFeed.tsx
+++ src/routes/app/RidesFeed.tsx
@@ -2,12 +2,25 @@ import Navbar from "../../components/Navbar";
 import RideFeedCard from "../../components/RideFeedCard";
 import "../../styles/rides-feed.css";
 import dummyData from "../../mockData/ridesFeedMockUp.json";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Button from "../../components/Button";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { AuthContext } from "../../context/auth";
 
 const RidesFeed = () => {
+    const { user } = useContext(AuthContext);
+    const [searchName, setSearchName] = useState("");
+    const [radius, setRadius] = useState(0);
 
-    const [radius, setRadius] = useState(50); // Default radius value
+    const { data: userData} = useQuery(FETCH_USER_QUERY, {
+        onCompleted() {
+            setSearchName(userData.getUser.locationName);
+            setRadius(userData.getUser.radius);
+        },
+        variables: {
+            username: user?.username,
+        },
+    });
 
     const [appliedFilters, setAppliedFilters] = useState<string[]>([]);
 
@@ -27,19 +40,48 @@ const RidesFeed = () => {
         });
     };
 
-
-
     const handleSliderChange = (event: any) => {
         const newRadius = event.target.value;
-        setRadius(newRadius);
-        
-    };
+        setRadius(parseInt(newRadius));
+    };  
 
-    const handleClick = () => {
-        console.log('Button clicked');
-    };    
+    const [setRegionUser, { loading, error, data }] = useMutation(SET_REGION_MUTATION);
 
+    const setRegion = async () => {
+        const searchStr = searchName.trim().toLowerCase();
+        const cachedQuery = await sessionStorage.getItem(searchStr);
+        var locName: string | null = null;
+        var locCoords: number[] | null = null;
+
+        if(cachedQuery) {
+            const cachedObj = JSON.parse(cachedQuery);
+            locName = cachedObj.locName;
+            locCoords = cachedObj.locCoords;
+        } else {
+            locName = searchStr;
+        }
+
+        const regionUser = await setRegionUser({
+            variables: {
+                username: user?.username,
+                locationName: locName,
+                locationCoords: locCoords,
+                radius: radius,
+            },
+        });
+
+        if (!cachedQuery) {
+            const storedObj = {
+                locName: regionUser.data.setRegion.locationName,
+                locCoords: regionUser.data.setRegion.locationCoords,
+            }
+            sessionStorage.setItem(searchStr, JSON.stringify(storedObj));
+        }
+    } 
     
+    const handleSubmit = async () => {
+        await setRegion();
+    };
 
     return (
         
@@ -107,22 +149,22 @@ const RidesFeed = () => {
 
                         <div className="rides-feed-filter-options" >
 
-                            <h5>Location radius</h5>                            
+                            <h5>Search Region</h5>                            
 
                             <div className="geolocation-radius-filter" >
-                                <label>ZIP Code</label>
-                                <input onChange={handleCheckboxChange} type="text" pattern="[0-9]{5}" title="Five digit zip code" />
+                                <label>Location:</label>
+                                <input onChange={e => {setSearchName(e.target.value)}} type="text" pattern="[0-9]{5}" title="Five digit zip code" value={searchName}/>
                             </div>
 
                             <label htmlFor="">
                                 Range: 
-                                <input type="range" min="1" max="100" value={radius} onChange={handleSliderChange} /> {radius == 100 ? radius + "+" : radius} miles
+                                <input type="range" min="1" max="100" value={radius} onChange={handleSliderChange} /> {radius == 100 ? radius + "+" : radius} km
                             </label>
 
                         </div>
 
                         <div className="rides-feed-filter-search" >
-                            <Button onClick={handleClick} type="primary">Search</Button>
+                            <Button onClick={handleSubmit} type="primary">Search</Button>
                         </div>
 
                         <div className="rides-feed-filters-applied">
@@ -163,5 +205,37 @@ const RidesFeed = () => {
         </>
     )
 };
+
+const FETCH_USER_QUERY = gql`
+  query getUser($username: String!) {
+    getUser(username: $username) {
+        locationName
+        locationCoords
+        radius
+    }
+  }
+`;
+
+const SET_REGION_MUTATION = gql`
+    mutation Mutation(
+        $username: String!
+        $locationName: String
+        $locationCoords: [Float]
+        $radius: Float
+    ) {
+        setRegion(
+            setRegionInput: {
+                username: $username
+                locationName: $locationName
+                locationCoords: $locationCoords
+                radius: $radius
+            }
+        ) {
+            locationName
+            locationCoords
+            radius
+        }
+    }
+`
 
 export default RidesFeed;
